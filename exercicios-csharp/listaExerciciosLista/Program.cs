723a78883d0b735cc8b17f641c60de5de562b16d
Felipe Grochovski <fgrochovski@gmail.com>
2025-02-11 11:40:39 -0300

    Add files via upload

--- exercicios-csharp/listaExerciciosLista/Program.cs
+++ exercicios-csharp/listaExerciciosLista/Program.cs
@@ -12,7 +12,7 @@ namespace CadastroAnimais
     {
         static void Main(string[] args)
         {
-            cadastroAnimais();
+            cadastroLoja();
             Console.ReadKey();
         }
 
@@ -67,20 +67,16 @@ namespace CadastroAnimais
 
                     case 4:
                         Console.WriteLine("** Maior animal **");
-                        for (int i = 0; i < animal.Count; i++)
-                        {
-                           
-                            Console.WriteLine($"Maior animal: {animal.Max()}");
-                        }
+
+                        Console.WriteLine($"Maior animal: {animal.Max()}");
                         Console.ReadKey();
                         Console.Clear();
                         break;
 
                     case 5:
-                        for (int i = 0; i < animal.Count; i++)
-                        {
-                            Console.WriteLine($"Menor animal: {animal.Min()}");
-                        }
+                        Console.WriteLine($"Menor animal: {animal.Min()}");
+                        Console.ReadKey();
+                        Console.ReadLine();
                         break;
 
                     case 6:
@@ -100,6 +96,73 @@ namespace CadastroAnimais
                 }
             }
 
+        }
+
+        static void cadastroLoja()
+        {
+            List<string> produtos = new List<string>();
+            List<double> preco = new List<double>();
+
+            while (true)
+            {
+
+                Console.WriteLine("1 - Cadastrar produto e preço");
+                Console.WriteLine("2 - Ver produtos e seu preço");
+                Console.WriteLine("3 - Produto mais caro");
+                Console.WriteLine("4 - Media de preço e produtos");
+                Console.WriteLine("5 - Comprar produto (perguntar a quantidade e o valor)");
+                Console.WriteLine("6 - Alterar produto (nome preço)");
+                Console.WriteLine("0 - Sair");
+                
+
+                int menu = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                switch (menu)
+                {
+                    case 1:
+                        Console.Write("Informe um produto:");
+                        produtos.Add(Console.ReadLine());
+                        Console.Write("Informe o preço do produto: R$");
+                        preco.Add(Convert.ToDouble(Console.ReadLine()));
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case 2:
+                        for (int i = 0; i < produtos.Count && i < preco.Count; i++)
+                        {
+                            Console.WriteLine($"{produtos[i]} R${preco[i]}");
+                        }
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+
+                    case 3:
+
+                        break;
+
+                    case 4:
+
+                        break;
+
+                    case 5:
+
+                        break;
+
+                    case 6:
+
+                        break;
+
+                    case 0:
+                        break;
+
+                    default:
+
+                        break;
+
+                }
+
+            }
         }
     }
 }
