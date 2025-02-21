345b7af2a25d354e2f62fd1a45373ee7dcaede6e
Felipe Grochovski <fgrochovski@gmail.com>
2025-02-21 11:28:29 -0300

    Add files via upload

---
+++ exercicios-mysql/lista-exercicios-abstracao/petshop.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE petshop;
+USE petshop;
+
+CREATE TABLE animal(
+	idAnimal INT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    especie VARCHAR(50) NOT NULL,
+    raca VARCHAR(50) NOT NULL,
+    peso DOUBLE NOT NULL,
+    altura FLOAT NOT NULL,
+    cor VARCHAR(50) NOT NULL,
+    pelagem VARCHAR(50) NOT NULL,
+    comportamento VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE servico(
+	
+);
