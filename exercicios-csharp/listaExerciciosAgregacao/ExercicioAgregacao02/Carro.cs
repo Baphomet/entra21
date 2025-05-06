aee18524084489137fa7a540d181f4551e4ae013
Felipe Grochovski <fgrochovski@gmail.com>
2025-05-06 16:56:38 -0300

    Add files via upload

---
+++ exercicios-csharp/listaExerciciosAgregacao/ExercicioAgregacao02/Carro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioAgregacao02
+{
+    internal class Carro
+    {
+        public Motor Motor;
+    }
+}
---
+++ exercicios-csharp/listaExerciciosAgregacao/ExercicioAgregacao02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioAgregacao02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Motor motor = new Motor();
+            motor.Potencia = 1;
+            motor.Tipo = "SS";
+            
+            Carro carro = new Carro();
+             carro.Motor = motor;
+        }
+    }
+}
