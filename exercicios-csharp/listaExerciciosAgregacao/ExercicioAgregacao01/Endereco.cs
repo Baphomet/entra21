4a1a60d5b7f302a7d40da3bd97e6e74d31d33923
Felipe Grochovski <fgrochovski@gmail.com>
2025-05-08 21:09:34 -0300

    Add files via upload

---
+++ exercicios-csharp/listaExerciciosAgregacao/ExercicioAgregacao01/Endereco.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioAgregacao01
+{
+    internal class Endereco
+    {
+        public string Rua;
+        public int Numero;
+        public string Cidade;
+    }
+}
---
+++ exercicios-csharp/listaExerciciosAgregacao/ExercicioAgregacao01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioAgregacao01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Endereco endereco = new Endereco();
+            endereco.Rua = "Sete de Setembro";
+            endereco.Numero = 34;
+
+            Pessoa pessoa = new Pessoa();
+            pessoa.Endereco = endereco;
+
+            Console.WriteLine(pessoa.Endereco.Rua);
+        }
+    }
+}
