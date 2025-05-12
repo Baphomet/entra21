8ee4c149a7d7ce0042aaebf317f21333bfdabc90
Felipe Grochovski <fgrochovski@gmail.com>
2025-05-11 23:53:53 -0300

    Add files via upload

---
+++ exercicios-csharp/listaExerciciosAgregacao/ExercicioAgregacao03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioAgregacao03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Disciplina disciplina = new Disciplina();
+            disciplina.Materia = "Matematica";
+
+            Professor professor = new Professor();
+            professor.Disciplina = disciplina;
+        }
+    }
+}
