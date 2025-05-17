aa3eb3b74b4e63659ca05e33b16fc6ce367ab459
Felipe Grochovski <fgrochovski@gmail.com>
2025-05-16 23:23:29 -0300

    Add files via upload

---
+++ exercicios-csharp/listaExerciciosAgregacao/ExercicioAgregacao04/Processador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioAgregacao04
+{
+    internal class Processador
+    {
+        public string Marca;
+        public int Velocidade;
+
+    public Processador(string marca, int velocidade)
+        {
+            this.Marca = marca;
+            this.Velocidade = velocidade;
+        }
+    }
+}
---
+++ exercicios-csharp/listaExerciciosAgregacao/ExercicioAgregacao04/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioAgregacao04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Processador processador = new Processador("Ryzen", 600);
+            
+
+            Computador computador = new Computador();
+            computador.Processador = processador;
+
+            Console.WriteLine(computador.Processador.Marca);
+        }
+    }
+}
